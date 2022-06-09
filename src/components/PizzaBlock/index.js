403227d3a94c7296a9b6b11478004389d9502a83
mitriifd <idimka9@gmail.com>
2022-06-09 18:28:44 +0500

    redux-логика для корзины, скрытие/показ popup-сортировки

--- src/components/PizzaBlock/index.js
+++ src/components/PizzaBlock/index.js
@@ -1,10 +1,30 @@
 import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-function PizzaBlock({ title, price, imageUrl, sizes, types }) {
+import { addItem } from '../../redux/slices/cartSlice';
+
+function PizzaBlock({ id, title, price, imageUrl, sizes, types }) {
+	const dispatch = useDispatch();
+	const cartItem = useSelector((state) =>
+		state.cart.items.find((obj) => obj.id === id)
+	);
+    const addedCount = cartItem ? cartItem.count : 0;
 	const [activeType, setActiveType] = useState(0);
 	const [activeSize, setActiveSize] = useState(0);
-
 	const typeNames = ['тонкое', 'традиционное'];
+
+	const onClickAdd = () => {
+		const item = {
+			id,
+			title,
+			price,
+			imageUrl,
+			type: typeNames[activeType],
+            size: sizes[activeSize]
+		};
+
+		dispatch(addItem(item));
+	};
 
 	return (
 		<div className='pizza-block'>
@@ -36,7 +56,10 @@ function PizzaBlock({ title, price, imageUrl, sizes, types }) {
 			</div>
 			<div className='pizza-block__bottom'>
 				<div className='pizza-block__price'>{price}</div>
-				<button className='button button--outline button--add'>
+				<button
+					onClick={onClickAdd}
+					className='button button--outline button--add'
+				>
 					<svg
 						width='12'
 						height='12'
@@ -50,7 +73,7 @@ function PizzaBlock({ title, price, imageUrl, sizes, types }) {
 						/>
 					</svg>
 					<span>Добавить</span>
-					<i>0</i>
+					{addedCount > 0 && <i>{addedCount}</i>}
 				</button>
 			</div>
 		</div>
--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setSort } from '../redux/slices/filterSlice';
 
@@ -14,6 +14,7 @@ export const sortList = [
 function Sort() {
 	const dispatch = useDispatch();
 	const sort = useSelector((state) => state.filter.sort);
+	const sortRef = useRef();
 
 	const [open, setOpen] = useState(false);
 
@@ -22,8 +23,22 @@ function Sort() {
 		setOpen(!open);
 	};
 
+	useEffect(() => {
+		const handlClickOutside = (e) => {
+			if (!e.path.includes(sortRef.current)) {
+				setOpen(false);
+			}
+		};
+
+		document.body.addEventListener('click', handlClickOutside);
+
+        return () => {
+            document.body.removeEventListener('click', handlClickOutside);
+        }
+	}, []);
+
 	return (
-		<div className='sort'>
+		<div ref={sortRef} className='sort'>
 			<div className='sort__label'>
 				<svg
 					width='10'
