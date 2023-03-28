213958b4e8f88285d0c518059f16aed9628bd9c1
Dmitriifd <idimka9@gmail.com>
2023-03-29 00:57:02 +0500

    change linnk

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -28,7 +28,7 @@ function Header() {
 	return (
 		<div className='header'>
 			<div className='container'>
-				<Link to='/'>
+				<Link to='https://dmitriifd.github.io/react-pizza'>
 					<div className='header__logo'>
 						<img width='38' src={logoSvg} alt='Pizza logo' />
 						<div>
